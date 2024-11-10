b25b4f45d6a482b112190f2f96d4d25ab55dd6dc
Diego Defilippi <defsan35@gmail.com>
2024-11-10 11:06:40 -0500

    feat: Modify WeatherResponse for Weather model

--- lib/home/models/weather.dart
+++ lib/home/models/weather.dart
@@ -1,11 +1,15 @@
-class WeatherResponse {
+import 'package:ayni_mobile_app/home/models/city.dart';
+
+class Weather {
   final double temperature;
   final String description;
   final String iconId;
+  final City city;
 
-  WeatherResponse({
+  Weather({
     required this.temperature,
     required this.description,
     required this.iconId,
+    required this.city,
   });
 }
